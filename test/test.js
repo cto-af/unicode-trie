c49b5dfd509d507c3f3b07e3fd537aafdaaa8018
Joe Hildebrand <joe-github@cursive.net>
2024-03-06 13:18:24 -0700

    Add coverage for fromBase64

--- test/test.js
+++ test/test.js
@@ -306,3 +306,55 @@ describe('unicode trie', () => {
     assert.match(m, /export const Foo/);
   });
 });
+
+// Unicode 15.1, fflate compressed
+const EastAsianWidthNew = `\
+AAAEAAAAAAC1AgAAH4sIAALJ6GUCA+2aP0gfMRTHz59tqbUUuhQKHVpoKXRxKg4OCi6Ki6A4
+CC7iJE7iJCKIOCgKgrg5iLrpppujkzgoqJOgLuKgg+giCOI3mpN43P0u5y8vyXnvBx+Sy7+X
+vHtJXi6/5VIQrIJ1sAnC5yKFTDL7CqyPYnBksK1jcJaQd1Fh21fgVsbvQXV1EHwAX8A38AP8
+Bv9AHRDl/iNskHEbNEtZbZZkdkBOOF+7lXiUXuTtyng/4oOy7ECkzjCeR5W0cSU+hbioK+Kz
+ZWQxDGOWuP2ZYRiGYRiGYfLGvDxHLlg8o+uyQtAnfucMwzD2KHm4tzAM4/f9ZyXfaalYk/c4
+Gwg3Y9a18F5HRdzZbIEdD9bBz7VPTH9yQ1OQncWPQTBX9cQp4j9rXua34nkykhZlG/k1kB+2
+44JWD+QXem0p+J3pocd9E/fjj/+FKLiveKJpo+codwluMtr0QQ50cJdgA8G7+PT3kfRaPItx
+fkX4PaaO0MMvpIvwL8Lwfxt1iNfLdLFnN8q4SpNMa0HYDrrAtdLnHqVOp5QdPvfheSBhDCG3
+Gva/B4bQzp+Y/kV9oBGUGQMTYAbMp8h/Cz5gnhF2tVSqeoZ//CvyT8wAl+j20ZVeTJc18Y6S
+dONqNaMcf9a2KeyPUm9U9UyOr1xbunLK1afQgdqvuLJZ5k1SfQr7obbJNJ2k6c30euZSX7bn
+P4XNpNm2L/s/lSxf/J08+Fs+9cGVTkzKM9GWbdu1LcPlXM/LechF/22Mv9L20s4+NvRGka+r
+b518inHb9Glcz/vX6DyprA9+f5LtmfjWQLFe+PCth0L2a/wtV/uVi/nt8zt1eZ6hlG167afy
+F/P0Lnw+V5ne+2zu1S6/+Wf1l3X2X905aNr3cnlec+HfU+1/vq3xVOuyaf/c9JrxAKGklNHg
+SQAAH4sIAALJ6GUCA4tW8lPSUYpUigUA0d8CLAkAAAA=`;
+
+// Unicode 15.1, brotli-compressed
+const EastAsianWidthOld = `\
+AAAEAAAAAABQAgAAG99JMB6Jsa2CPeYryr2sfj99GygBKkJSf6IyVUDkyleXutZFVhEqYAoA
+mJtrCtRv0ZrqSbitMeeLQiLoCh3hSuDeR5eMedj/slD10Y0R5iZ4+1ElV2MAzTz7ie96VxgM
+BoNCIFAIDAKBQiFQGMxQI8EMpKCeSXI0syhuvoVSwY4LVLCPs8kIN7NFeMQnCP+GjwxUoAMj
+WMEBrmOpccOHwuuBEDTEyKsphJvIPvu382JWAW5AzGWi59pyqq/JBkL7AAd6X0lutmWoXTK8
+jsjwhoAZJtQw4MSUXienlP8GngeVYoXLhKPpn04ds0TKLcS+/L9zzLE4iZ7Sb4+w85UVDsyg
+8c3APOBwzY6Po64NSwzM3jzeUfk3MNAepfJMo6KX2/yfptV2McXQ0c6pKaP7H0DHF9wCL2LA
+VpoRrvPjw+JMsFaoUXSrAnX1xaZnLg2yNZtBrnpYAUeAA4EeR5pISE8+6yfznmtTJd4rBUSh
+Ldz0MwSs1VWmteyH40rrGprQhSGL0EL41q07Vp3scFtX1QxMl6kCS6s6iRLFngIY4zQBuAm3
+iesqIJrwHuAuIdQEG3mAAXfFAnIa/rPEfFOMA1xncd81oA89/JMZ3XP+FLBSKg3FUOLPLpUA
+u53UYL1UHlPJOKMn20b8A2OVN5LoVDAfwnL8t/KhRIdlBmjyhnoCxO2HJTDqLN2BuIRqOWjz
+3FoN8fSiXC6+0EnqSiK028ZajciD2V3UhLc0/o1FsII8qw3w1DgGjoNkBjrNMoxlu/GnvnVL
+yNjUsU0v3smU+gsEgFsiTiIsIlkiXQM=`;
+
+describe('compatibility', () => {
+  it('reads the current format from base64', () => {
+    const trie = UnicodeTrie.fromBase64(EastAsianWidthNew);
+    assert.equal(trie.get(0x3000), 1);
+  });
+  it('reads the current format from base64 without Buffer', () => {
+    const buf = globalThis.Buffer;
+    globalThis.Buffer = {};
+    const trie = UnicodeTrie.fromBase64(EastAsianWidthNew);
+    assert.equal(trie.get(0x3000), 1);
+    globalThis.Buffer = buf;
+  });
+  it('rejects old versions', () => {
+    assert.throws(
+      () => UnicodeTrie.fromBase64(EastAsianWidthOld),
+      /Error: invalid gzip data/
+    );
+  });
+});
