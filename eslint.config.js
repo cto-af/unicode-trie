d86325fb34cc4631bea494e9a5aa4cc5e2a222e7
Joe Hildebrand <joe-github@cursive.net>
2025-03-18 09:29:11 -0500

    Update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,17 @@
 import es6 from '@cto.af/eslint-config/es6.js';
-import globals from '@cto.af/eslint-config/globals.js';
-import mocha from 'eslint-plugin-mocha';
+import mocha from '@cto.af/eslint-config/mocha.js';
 
 export default [
-  {
-    ignores: [
-      '**/*.d.ts',
-    ],
-  },
   ...es6,
+  ...mocha,
   {
     files: ['**/*.js'],
     rules: {
+      // Don't want to import from Buffer, so that this code can test for
+      // Buffer being defined at runtime, and fall back on worse code in the
+      // browser.
       'n/prefer-global/buffer': 'off',
       'prefer-named-capture-group': 'off',
-    },
-  },
-  {
-    files: ['test/*.js'],
-    plugins: {
-      mocha,
-    },
-    languageOptions: {
-      globals: globals.mocha,
-    },
-    rules: {
-      ...mocha.configs.recommended.rules,
-      'mocha/no-mocha-arrows': 'off',
     },
   },
 ];
